51c137d4c02e42806143c8c40e1a44e4a0e8001e
Breno Farias <41799984+brenofarias@users.noreply.github.com>
2024-03-31 20:22:44 -0300

    Criando a classe ContextBase
    Configurado a string de conexão

---
+++ Infraestructure/Configuration/ContextBase.cs
@@ -0,0 +1,34 @@
+﻿using Entities.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infraestructure.Configuration
+{
+    public class ContextBase : DbContext
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options) 
+        {
+        }
+        
+        public DbSet<Produto> Produto {  get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if(!optionsBuilder.IsConfigured) 
+            {
+                optionsBuilder.UseSqlServer(GetStringConectionConfig());
+                base.OnConfiguring(optionsBuilder);
+            }
+        }
+
+        private string GetStringConectionConfig()
+        {
+            string srtCon = "Data Source=FARIASTEIN\\SQLEXPRESS;Network Library=DBMSSOCN;Initial Catalog=DDD_ECOMERCE;Integrated Security=False;User ID=FARIASTEIN\breno;Password=error404;";
+            return srtCon;
+        }
+    }
+}
