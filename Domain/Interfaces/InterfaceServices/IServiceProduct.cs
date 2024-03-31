cd9d22ecf6146603013114e53a6d9afc640fbf60
Breno Farias <41799984+brenofarias@users.noreply.github.com>
2024-03-31 18:54:20 -0300

    Criado a interface ServiceProduct.
    ServiceProduct terá os metodos para adicionar e atualizar, apesar de já existir esses metodos na interface Generic, pois o produto terá algumas validações especificas

---
+++ Domain/Interfaces/InterfaceServices/IServiceProduct.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.InterfaceServices
+{
+    public class IServiceProduct
+    {
+        // Criado os metodos de adicionar e atualizar pois terá validações especificas
+        Task AddProduct(Produto produto);
+        Task UpdateProduct(Produto produto);
+    }
+}
