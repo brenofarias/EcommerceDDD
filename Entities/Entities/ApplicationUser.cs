f4f528d2026ea58bf754499e0324023f607fd341
Breno Farias <brenofarias.sousa@gmail.com>
2024-04-24 20:11:59 -0300

    Colocando as propriedades com null

--- Entities/Entities/ApplicationUser.cs
+++ Entities/Entities/ApplicationUser.cs
@@ -15,45 +15,45 @@ namespace Entities.Entities
         [Column("USR_CPF")]
         [MaxLength(50)]
         [Display(Name = "CPF")]
-        public string CPF { get; set; }
+        public string? CPF { get; set; }
 
         [Column("USR_IDADE")]
         [Display(Name = "Idade")]
-        public int Idade {  get; set; }
+        public int? Idade {  get; set; }
 
         [Column("USR_NOME")]
         [MaxLength(255)]
         [Display(Name = "Nome")]
-        public string Nome { get; set; }
+        public string? Nome { get; set; }
 
         [Column("USR_CEP")]
         [MaxLength(15)]
         [Display(Name = "CEP")]
-        public string Cep { get; set; }
+        public string? Cep { get; set; }
 
         [Column("USR_ENDERECO")]
         [MaxLength(255)]
         [Display(Name = "Endereço")]
-        public string Endereco { get; set; }
+        public string? Endereco { get; set; }
 
         [Column("USR_COMPLEMENTO_ENDERECO")]
         [MaxLength(450)]
         [Display(Name = "Complemento de Endereço")]
-        public string ComplementoEndereco { get; set; }
+        public string? ComplementoEndereco { get; set; }
 
         [Column("USR_CELULAR")]
         [MaxLength(20)]
         [Display(Name = "Celular")]
-        public string Celular { get; set; }
+        public string? Celular { get; set; }
 
         [Column("USR_TELEFONE")]
         [MaxLength(20)]
         [Display(Name = "Telefone")]
-        public string Telefone { get; set; }
+        public string? Telefone { get; set; }
 
         [Column("USR_ESTADO")]
         [Display(Name = "Estado")]
-        public bool Estado { get; set; }
+        public bool? Estado { get; set; }
 
         [Column("USR_TIPO")]
         [Display(Name = "Tipo")]
